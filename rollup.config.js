a76a3677e40732b007e2774a3c8872cabb4b7608
mstssk <mstssk+github@gmail.com>
2020-05-25 01:12:32 +0900

    Format files by Prettier.
    npx prettier --write rollup.config.js src/index.ts src/manifest.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,37 @@
-import copy from 'rollup-plugin-copy';
-import typescript from '@rollup/plugin-typescript';
-import cleaner from 'rollup-plugin-cleaner';
+import copy from "rollup-plugin-copy";
+import typescript from "@rollup/plugin-typescript";
+import cleaner from "rollup-plugin-cleaner";
 
 const OUT_DIR = "./dist";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
     dir: OUT_DIR,
-    format: 'es'
+    format: "es",
   },
   plugins: [
     cleaner({
-      targets: [OUT_DIR]
+      targets: [OUT_DIR],
     }),
     typescript(),
     copy({
       targets: [
         {
-          src: 'src/img',
+          src: "src/img",
           dest: OUT_DIR,
         },
         {
-          src: 'src/manifest.json',
+          src: "src/manifest.json",
           dest: OUT_DIR,
           transform(content) {
             const manifest = JSON.parse(content);
             manifest.description = process.env.npm_package_description;
             manifest.version = process.env.npm_package_version;
             return JSON.stringify(manifest, null, 2);
-          }
+          },
         },
-      ]
-    })
-  ]
-}
+      ],
+    }),
+  ],
+};
