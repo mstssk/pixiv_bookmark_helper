3baf128f075543f5445a4b8258085b2442e5da7b
mstssk <mstssk+github@gmail.com>
2021-06-09 21:03:16 +0900

    Migrate manifest.json into v3

--- src/background.ts
+++ src/background.ts
@@ -1,26 +1,28 @@
-chrome.contextMenus.create({
-  id: "bookmark_illust",
-  title: "イラスト・マンガのブックマークを開く",
-  contexts: ["page_action"],
-});
+chrome.contextMenus.removeAll(() => {
+  chrome.contextMenus.create({
+    id: "bookmark_illust",
+    title: "イラスト・マンガのブックマークを開く",
+    contexts: ["page_action"],
+  });
 
-chrome.contextMenus.create({
-  id: "bookmark_novel",
-  title: "小説のブックマークを開く",
-  contexts: ["page_action"],
-});
+  chrome.contextMenus.create({
+    id: "bookmark_novel",
+    title: "小説のブックマークを開く",
+    contexts: ["page_action"],
+  });
 
-chrome.contextMenus.onClicked.addListener((info) => {
-  let url: string;
-  switch (info.menuItemId) {
-    case "bookmark_illust":
-      url = "https://www.pixiv.net/bookmark.php";
-      break;
-    case "bookmark_novel":
-      url = "https://www.pixiv.net/novel/bookmark.php";
-      break;
-    default:
-      return; // NOOP
-  }
-  chrome.tabs.create({ url });
+  chrome.contextMenus.onClicked.addListener((info) => {
+    let url: string;
+    switch (info.menuItemId) {
+      case "bookmark_illust":
+        url = "https://www.pixiv.net/bookmark.php";
+        break;
+      case "bookmark_novel":
+        url = "https://www.pixiv.net/novel/bookmark.php";
+        break;
+      default:
+        return; // NOOP
+    }
+    chrome.tabs.create({ url });
+  });
 });
