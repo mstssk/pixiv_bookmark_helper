dad7da9600a0233be8068097165d3c46118c76ef
mstssk <mstssk+github@gmail.com>
2021-06-09 20:48:48 +0900

    Delete unnecessary code.

--- src/background.ts
+++ src/background.ts
@@ -1,43 +1,26 @@
-import { promisify } from "./util";
+chrome.contextMenus.create({
+  id: "bookmark_illust",
+  title: "イラスト・マンガのブックマークを開く",
+  contexts: ["page_action"],
+});
 
-type MENU_ITEM = "bookmark_illust" | "bookmark_novel";
+chrome.contextMenus.create({
+  id: "bookmark_novel",
+  title: "小説のブックマークを開く",
+  contexts: ["page_action"],
+});
 
-declare namespace chrome.contextMenus {
-  interface CreateProperties {
-    id?: MENU_ITEM;
+chrome.contextMenus.onClicked.addListener((info) => {
+  let url: string;
+  switch (info.menuItemId) {
+    case "bookmark_illust":
+      url = "https://www.pixiv.net/bookmark.php";
+      break;
+    case "bookmark_novel":
+      url = "https://www.pixiv.net/novel/bookmark.php";
+      break;
+    default:
+      return; // NOOP
   }
-  interface OnClickData {
-    menuItemId: MENU_ITEM;
-  }
-}
-
-(async function () {
-  await promisify(chrome.contextMenus.removeAll)();
-
-  chrome.contextMenus.create({
-    id: "bookmark_illust",
-    title: "イラスト・マンガのブックマークを開く",
-    contexts: ["page_action"],
-  });
-
-  chrome.contextMenus.create({
-    id: "bookmark_novel",
-    title: "小説のブックマークを開く",
-    contexts: ["page_action"],
-  });
-
-  chrome.contextMenus.onClicked.addListener((info) => {
-    let url = "";
-    switch (info.menuItemId) {
-      case "bookmark_illust":
-        url = "https://www.pixiv.net/bookmark.php";
-        break;
-      case "bookmark_novel":
-        url = "https://www.pixiv.net/novel/bookmark.php";
-        break;
-      default:
-        return; // NOOP
-    }
-    chrome.tabs.create({ url });
-  });
-})();
+  chrome.tabs.create({ url });
+});
